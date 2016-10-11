02d5951d51f7f64242c527d599fd6367b06cb780
ContenteTV <h2410258@mvrht.com>
2016-10-11 00:28:18 -0300

    Update version.js

--- bot/commands/bot/version.js
+++ bot/commands/bot/version.js
@@ -1,7 +1,7 @@
 var pkg = require(process.cwd() + "/package.json");
 
 module.exports = function(bot, db) {
-    bot.sendChat("SteveBot version: " + pkg.version);
+    bot.sendChat("ContenteBot versão: " + pkg.version);
 };
 
 module.exports.extraCommands = ["v", "ver"];
